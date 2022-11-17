c112859aba4e9e205c479f781125a29881bddb5c
Jesus Angel La Rosa Trinidad <ogti_temp84@produce.gob.pe>
2022-11-16 21:31:07 -0500

    .

--- Prod.RutaDigital.Presentacion.Angular/ClientApp/src/app/perfil-avance/perfil-avance.component.ts
+++ Prod.RutaDigital.Presentacion.Angular/ClientApp/src/app/perfil-avance/perfil-avance.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { CatalogoPremiosRepository } from '../catalogo-premios/catalogo-premios.repository';
+import { PremioResponse } from '../catalogo-premios/interfaces/premio.response';
 
 @Component({
   selector: 'app-perfil-avance',
@@ -6,7 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: [],
 })
 export class PerfilAvanceComponent implements OnInit {
-  constructor() {}
+  nuevosPremios: PremioResponse[];
+  constructor(
+    private catalogoPremios: CatalogoPremiosRepository,
+    private sanitizer: DomSanitizer,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
+
+  listarPremioNuevo = () => {
+    var request: any = {
+      CantReg: 10,
+      IdListCatalogo: 1,
+    };
+    this.catalogoPremios.listarPremio(request).subscribe({
+      next: (data: PremioResponse[]) => {
+        this.nuevosPremios = data.map((nuevoPremio) => {
+          const objectURL = 'data:image/png;base64,' + nuevoPremio.numArray;
+          nuevoPremio.imagenPremio =
+            this.sanitizer.bypassSecurityTrustUrl(objectURL);
+
+          let objectURLTipo =
+            'data:image/png;base64,' + nuevoPremio.fotoTipoArray;
+          nuevoPremio.imagenTipoPremio =
+            this.sanitizer.bypassSecurityTrustUrl(objectURLTipo);
+          return nuevoPremio;
+        });
+      },
+      error: (err) => {},
+    });
+  };
+
+  loQuiero = (item) => {
+    this.router.navigate(['/catalogo-premios/premio', item]);
+  };
 }
