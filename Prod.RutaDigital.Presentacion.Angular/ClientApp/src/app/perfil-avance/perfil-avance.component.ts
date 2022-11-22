796da3922387888a94c9eb4fb740141fef0943c8
Jesus Angel La Rosa Trinidad <ogti_temp84@produce.gob.pe>
2022-11-21 22:45:11 -0500

    .

--- Prod.RutaDigital.Presentacion.Angular/ClientApp/src/app/perfil-avance/perfil-avance.component.ts
+++ Prod.RutaDigital.Presentacion.Angular/ClientApp/src/app/perfil-avance/perfil-avance.component.ts
@@ -11,13 +11,105 @@ import { PremioResponse } from '../catalogo-premios/interfaces/premio.response';
 })
 export class PerfilAvanceComponent implements OnInit {
   nuevosPremios: PremioResponse[];
+  ListNivelAutodiagnostico: Array<any>;
+  ListCapacitando: Array<any>;
+  ListNivel: Array<any>;
   constructor(
     private catalogoPremios: CatalogoPremiosRepository,
     private sanitizer: DomSanitizer,
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ListNivelAutodiagnostico = [
+      {
+        numero_avance:"1",
+        nivel:"Nivel Básico",
+        tipo_nivel:"Marketing Digital"
+      },
+      {
+        numero_avance:"2",
+        nivel:"Nivel Incial",
+        tipo_nivel:"Comercio electrónico"
+      },
+      {
+        numero_avance:"3",
+        nivel:"Nivel Básico",
+        tipo_nivel:"Gestión Empresarial"
+      },
+      {
+        numero_avance:"4",
+        nivel:"Nivel Básico",
+        tipo_nivel:"Finanzas"
+      },
+      {
+        numero_avance:"5",
+        nivel:"Nivel Incial",
+        tipo_nivel:"Medios de pago"
+      },
+      {
+        numero_avance:"6",
+        nivel:"Nivel Incial",
+        tipo_nivel:"Análisis de datos"
+      },
+    ];
+
+    this.ListCapacitando = [
+      {
+        nivel:"Nivel Básico",
+        tipo_nivel:"Gestión Empresarial",
+        recomendacion:"Recomendación #4",
+        porcentaje:"0%"
+      },
+      {
+        nivel:"Nivel Incial",
+        tipo_nivel:"Gestión Empresarial",
+        recomendacion:"Recomendación #33",
+        porcentaje:"10%"
+      },
+      {
+        nivel:"Nivel Básico",
+        tipo_nivel:"Gestión Empresarial",
+        recomendacion:"Recomendación #44",
+        porcentaje:"20%"
+      },
+      {
+        nivel:"Nivel Incial",
+        tipo_nivel:"Gestión Empresarial",
+        recomendacion:"Recomendación #24",
+        porcentaje:"30%"
+      },
+      {
+        nivel:"Nivel Básico",
+        tipo_nivel:"Gestión Empresarial",
+        recomendacion:"Recomendación #785",
+        porcentaje:"40%"
+      },
+    ];
+
+    this.ListNivel = [
+      {
+        nivel:"Inicial",
+        flag:false,
+      },
+      {
+        nivel:"Básico",
+        flag:true,
+      },
+      {
+        nivel:"Intermedio",
+        flag:false,
+      },
+      {
+        nivel:"Avanzado",
+        flag:false,
+      },
+      {
+        nivel:"Experto",
+        flag:false,
+      }
+    ];
+  }
 
   listarPremioNuevo = () => {
     var request: any = {
